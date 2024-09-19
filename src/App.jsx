c12cb2515e750e29acd55c816a8d442ce4edfae2
mr-dev360 <maheshdgaikwa18@gmail.com>
2024-09-19 22:09:31 +0530

    say hello to deploy

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+
+    <div className='w-full bg-orange-400 p-3 mt-7 text-center'>
+      <h1 className='text-white text-4xl'>master.dhanesh bhaiya</h1>
+
+    </div>
+  )
+}
+
+export default App
+
+// ek se jyad ahai to 
+// export const b 
+// export const b 
+
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx' // name kuch bhi liko yaha q ki ana to ek he hai 
+import './index.css'
+// import { a, b } from './ agar ek se jyada kr rahe hai 
+
+createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
