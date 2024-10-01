40506946f9f29d8d768bb5da116f83f753982bc6
mr-dev360 <maheshdgaikwa18@gmail.com>
2024-10-02 00:17:05 +0530

    component change

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { useState } from "react";
+import Nav from "./components/nav.jsx";
+import Hero from "./components/Hero.jsx";
 
 function App() {
 
 
   return (
-   <div className="w-full h-screen flex ">
-
-    <div className="left w-[35%] h-full bg-stone-900 flex items-center justify-between bg-[url('https://images.unsplash.com/photo-1649930536248-df58fd1f54f8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAzfHxkZXNpZ25lcnxlbnwwfHwwfHx8MA%3D%3D')]">
-      <div className="text w-full h-full text-white p-10 absolute top-96 left-36 leading-10 ">
-        <h2 className="text-5xl mb-2">MY NAME IS</h2>
-        <h1 className=" text-8xl text-thin mb-5 font-extrabold">MAHE$H</h1>
-        <button className="py-4 px-5 bg-orange-400 text-2xl">I'M A UI UX DZ!NER & DEVELOPER</button>
-      </div>
-    </div>
-
-    <div className="right w-[70%] h-full">
-    <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1598387746216-506f6bd47aad?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGVzaWduZXJ8ZW58MHx8MHx8fDA%3D" alt="" />
-
-    </div>
-   </div>
+   <>
+    <Nav />
+    <Hero />
+   </>
   );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx"; // name kuch bhi liko yaha q ki ana to ek he hai
 import "./index.css";
-import Nav from "./components/nav.jsx";
 // import { a, b } from './ agar ek se jyada kr rahe hai
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Nav />
     <App />
   </>
 );
